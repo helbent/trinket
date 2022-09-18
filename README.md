8343830b097deb1bb6bb6401956dbe243cb8918a
helbent <109105035+helbent@users.noreply.github.com>
2022-09-18 16:13:11 -0600

    accelerometer

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Rename code to code.py and save in trinket drive
 To view the serial console:
 
 `screen /dev/ttyACM0 115200`
+
+circuitpy_display has all the libs in lib
---
+++ accel.py
@@ -0,0 +1,22 @@
+# SPDX-FileCopyrightText: 2019 Bryan Siepert for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
+
+from time import sleep
+import board
+import busio
+import adafruit_lsm303_accel
+import adafruit_lis2mdl
+
+i2c = busio.I2C(board.SCL, board.SDA)
+mag = adafruit_lis2mdl.LIS2MDL(i2c)
+accel = adafruit_lsm303_accel.LSM303_Accel(i2c)
+
+while True:
+    if accel.acceleration[2] > 0:
+        print("hello", accel.acceleration[2])
+    #print("Acceleration (m/s^2): X=%0.3f Y=%0.3f Z=%0.3f"%accel.acceleration)
+    #print("Magnetometer (micro-Teslas)): X=%0.3f Y=%0.3f Z=%0.3f"%mag.magnetic)
+    #print("")
+    sleep(0.2)
+
